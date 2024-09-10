f0e3ec62f5f5bd8a46c2a4807f9f27af554e7fdb
yoonsen <yoonsen@gmail.com>
2024-09-10 17:51:10 +0200

    første til Beatrice

---
+++ 00_🏠_Text_analysis.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from PIL import Image
+import urllib
+import pandas as pd
+import dhlab as dh
+import streamlit as st
+
+st.set_page_config(page_title="POLNET", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
+st.session_state.update(st.session_state)
+
+
+#st.session_state.update(st.session_state)
+
+
+# zotero_excel = "Zoterobibliotek eksportert_010420.xlsx"
+
+#### CODE FOR WELCOME SCREEN
+
+
+@st.cache_data()
+def corpus():
+    c = pd.read_excel("Beatrice_korpus.xlsx", index_col=0)
+    st.session_state['korpus'] = c
+    st.session_state['dhlabid'] = [int(x) for x in c.dhlabid]
+    c['url'] = c.urn.apply(lambda x: f"https://nb.no/items/{x}")
+    return c
+
+litt_korpus = corpus()
+st.dataframe(
+    litt_korpus[['url','title', 'authors','year']],
+    column_config={
+            "url": st.column_config.LinkColumn("URL")
+    },
+    hide_index=True
+)
+
+    
+        
